b728eeac2362927ae7ce7923f1bfa074af645a97
Karin Hsu <khsu0720@gmail.com>
2018-09-21 21:16:32 -0700

    testing image for gateway 502 error

--- proxyServer/index.js
+++ proxyServer/index.js
@@ -5,16 +5,12 @@ var cors = require('cors');
 
 app.use(cors());
 app.use('/:id', express.static(`${__dirname}/../client/dist`));
-// app.get((req, res) => {
-//   console.log("it works");
-//   res.send("hello world from server");
-// });
 
-app.all('/api/restaurant/reviews/*', (req, res) => res.redirect(`http://localhost:8080${req.path}`));
-app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://localhost:1177${req.path}`));
-app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://localhost:1170${req.path}`));
-app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://localhost:8888${req.path}`));
-app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://localhost:3004${req.path}`));
+app.all('/api/restaurant/reviews/*', (req, res) => res.redirect(`http://reviews-api.us-west-2.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://zagat-info2.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://zagat-suggestions-dev.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev6.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://rec-public-dev.us-west-1.elasticbeanstalk.com${req.path}`));
 
 // app.use((req, res) => { 
 //   console.log(req.path);
